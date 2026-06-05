0da1c75a90445d122c38558950353716b6227d5f
prajapatiayushi77-design <prajapatiayushi77@gmail.com>
2026-06-05 11:44:39 +0530

    my  javascript program

---
+++ basic.js
@@ -0,0 +1,14 @@
+let myname = "ayushi prajapati";
+let mygoal = "fullstack developer";
+let currentcomppany = "Ignite digital";
+
+console.log("my name is " + myname);
+console.log("my goal is " + mygoal);
+console.log("my current company is " + currentcomppany);
+
+let age = 21;
+let yearsToSenior = 5;
+
+console.log("I am " + age + " years old");
+console.log("In " + yearsToSenior + " years I will be a senior developer");
+console.log("That means at age " + (age + yearsToSenior) + " I will be earning 15-20 LPA");
